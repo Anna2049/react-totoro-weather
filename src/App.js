298b54e0f48459e3f91b6ce9c12f41d46a911303
Anna2049 <85588122+Anna2049@users.noreply.github.com>
2023-03-21 02:56:30 +0100

    App js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,27 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const savedPreferences = localStorage.getItem("TotoroWeatherApp")
-    ? JSON.parse(localStorage.getItem("TotoroWeatherApp"))
-    : null;
-  const units = savedPreferences.units ? savedPreferences.units : "metric";
-  const theme = savedPreferences.theme ? savedPreferences.theme : "nature";
-  const location = savedPreferences.location
-    ? savedPreferences.location
-    : {
-        name_short: "Kyiv",
-        address: "Kyiv, Ukraine, 02000",
-        coords: {
-          lat: 50.4501,
-          lng: 30.5234,
-        },
-      };
   const [loading, setLoading] = useState(false);
-  const [preferences, setPreferences] = useState({
-    units: units,
-    theme: theme,
-    location: location,
-    weatherData: weather,
-  });
+  const [preferences, setPreferences] = useState({});
   const [weatherData, setWeatherData] = useState(null);
   useEffect(() => {
     const loadApp = async () => {
       setLoading(true);
+      const savedPreferences = localStorage.getItem("TotoroWeatherApp")
+        ? JSON.parse(localStorage.getItem("TotoroWeatherApp"))
+        : null;
+      const units = savedPreferences.units ? savedPreferences.units : "metric";
+      const theme = savedPreferences.theme ? savedPreferences.theme : "nature";
+      const location = savedPreferences.location
+        ? savedPreferences.location
+        : {
+            name_short: "Kyiv",
+            address: "Kyiv, Ukraine, 02000",
+            coords: {
+              lat: 50.4501,
+              lng: 30.5234,
+            },
+          };
       const keyOpenWeather = "13e9496ba2a5643119025f905a5f6396";
       const url =
         "https://api.openweathermap.org/data/2.5/onecall" +
@@ -39,6 +34,11 @@ function App() {
         `&exclude=minutely&appid=${keyOpenWeather}`;
       console.log(url);
       const weather = await axios.get(url);
+      setPreferences({
+        units: units,
+        theme: theme,
+        location: location,
+      });
       setWeatherData(weather.data);
       setTimeout(() => {
         setLoading(false);
