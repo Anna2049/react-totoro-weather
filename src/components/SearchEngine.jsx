ddebae8ade00ff886c3ba32cf7dde5bd33f82309
Anna2049 <85588122+Anna2049@users.noreply.github.com>
2023-03-21 21:10:58 +0100

    empty google search input on place processed

--- src/components/SearchEngine.jsx
+++ src/components/SearchEngine.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import Autocomplete from "react-google-autocomplete";
 import axios from "axios";
 
 const SearchEngine = (props) => {
   const units = "metric"; //props.units;
   const updateLocation = props.updateLocation;
+  const autocompleteRef = useRef(null);
+
   function getOpenWeatherData(coords) {
-    //console.log("Sending request to OpenWeather...");
+    console.log("Sending request to OpenWeather...");
     const setWeatherData = props.updateWeatherData;
     const keyOpenWeather = "13e9496ba2a5643119025f905a5f6396";
     const url =
@@ -26,6 +28,7 @@ const SearchEngine = (props) => {
         setWeatherData(null);
       });
   }
+
   function processSelectedPlace(googlePlaceObj) {
     const location = {
       name_short: googlePlaceObj.address_components[0].long_name,
@@ -43,12 +46,15 @@ const SearchEngine = (props) => {
     localStorage.setItem("TotoroWeatherApp", JSON.stringify(savedPreferences));
     updateLocation(location);
     getOpenWeatherData(location.coords);
+    autocompleteRef.current.value = "";
   }
+
   return (
     <Autocomplete
       className="SearchEngine"
       apiKey={"AIzaSyCnMPWcmFo_Je-QnkZBUUNyIlaqwyBIBco"}
       onPlaceSelected={(place) => processSelectedPlace(place)}
+      ref={autocompleteRef}
     />
   );
 };
